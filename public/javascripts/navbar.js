565da8ee84e2bacb400c506571d599d369f2f3bd
unknown <alexanderntisko@gmail.com>
2020-12-29 18:44:42 -0300

    startintgProject

---
+++ public/javascripts/navbar.js
@@ -0,0 +1,28 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".menu-links");
+  const navLinks = document.querySelectorAll(".menu-links li");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("menu-active");
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.3s ease forwards ${
+          index / 7 + 0.4
+        }s`;
+        console.log(index / 7);
+      }
+    });
+    //Burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+navSlide();
+
+let x = document.querySelectorAll('.animationTitle');
+let y = document.querySelectorAll('.animationText');
+let w = document.querySelector('.me')
+let i;
+console.log(x.length)
